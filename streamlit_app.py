d6fec0ca3ad655f2f8f85ea5d29f186873ed432e
Omprakash <reguomprakash6661@gmail.com>
2026-03-01 20:55:59 +0530

    fixed model loading

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,19 +7,33 @@ from PIL import Image
 import gdown
 import os
 
+st.set_page_config(page_title="Rice Leaf Disease Detection", layout="centered")
 st.title("Rice Leaf Disease Detection 🌾")
 
 MODEL_PATH = "resnet50_finetuned.h5"
-FILE_ID = "1GPYJZLF87XS-J2KrnGmxMdxiLIR8rP18"
+
+# ✅ NEW WORKING FILE ID
+FILE_ID = "1qAZdvwPnxjovdfirN4MkYKotXi9s415e"
+
 GDRIVE_URL = f"https://drive.google.com/uc?id={FILE_ID}"
 
-# Download model once
-if not os.path.exists(MODEL_PATH):
-    with st.spinner("Downloading model..."):
-        gdown.download(GDRIVE_URL, MODEL_PATH, quiet=False)
 
-# Load model AFTER download
-model = load_model(MODEL_PATH)
+# ✅ MODEL LOADER (PRODUCTION SAFE)
+@st.cache_resource
+def load_my_model():
+
+    if not os.path.exists(MODEL_PATH):
+        try:
+            with st.spinner("🔽 Downloading model... Please wait once"):
+                gdown.download(GDRIVE_URL, MODEL_PATH, quiet=False)
+        except Exception as e:
+            st.error("❌ Model download failed. Check Google Drive link & permission.")
+            st.stop()
+
+    return load_model(MODEL_PATH)
+
+
+model = load_my_model()
 
 CLASSES = [
     "Brown Spot",
@@ -35,23 +49,23 @@ uploaded_file = st.file_uploader(
 )
 
 if uploaded_file is not None:
+
     image = Image.open(uploaded_file).convert("RGB")
     st.image(image, caption="Uploaded Image", width=400)
-    
+
     img = image.resize((224, 224))
     img = np.array(img)
     img = np.expand_dims(img, axis=0)
     img = preprocess_input(img)
 
     if st.button("Predict"):
+
         pred = model.predict(img)
         idx = np.argmax(pred)
         predicted_class = CLASSES[idx]
 
-
-        st.success(f"Prediction: {CLASSES[idx]}")
+        st.success(f"Prediction: {predicted_class}")
         st.info(f"Confidence: {pred[0][idx]*100:.2f}%")
-
 
         recommendations = {
             "Brown Spot": {
